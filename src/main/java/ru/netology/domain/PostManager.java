e0a09ef56a8dedbe503687d025420deb5cfabcff
Stasia-Ku <67242445+Stasia-Ku@users.noreply.github.com>
2020-09-03 20:48:58 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/domain/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+        return;
+    }
+}
